9e94624ccc111bd19783e6fe71d9faa873ed4910
Patrick S <25529827+Squaretrix@users.noreply.github.com>
2018-09-18 14:55:14 +0200

    Uncompleted
    No ArrayIndexOutOfBoundsException but it doesnt set ships

--- Field.java
+++ Field.java
@@ -24,11 +24,33 @@ public class Field {
 		int counter = 0;
 
 		while (counter < ships) {
-			counter++;
 			int x = (int) (Math.random() * field.length);
 			int y = (int) (Math.random() * field.length);
-			field[y][x] = 's';
+
+			if (field[y][x] != 's') {
+				for (int i = 0; i < 3; i++) {
+					for (int j = 0; j < 3; j++) {
+						if (field[i][j] > field.length || field[i][j] < field.length) {
+							System.out.println("im if");
+							continue;
+						} else {
+							if (field[y+1][x]=='s'||field[y-1][x-1]=='s') {
+								continue;
+							}else {
+								field[y][x] = 's';
+							}
+							
+						}
+					}
+				}
+
+				counter++;
+			} else {
+				System.out.println("gleich");
+				continue;
+			}
+
 		}
- 
+
 	}
 }
