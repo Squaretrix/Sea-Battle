398c8057494627ebfaec41bcdde4e64af32bffc2
Patrick S <25529827+Squaretrix@users.noreply.github.com>
2018-09-26 17:30:46 +0200

    COMPLETED
    Missing:
    Commentaries

--- Main.java
+++ Main.java
@@ -3,9 +3,14 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Field f = new Field();
-		
-		char[][] player1 = f.create(9,12);
-		f.place(player1, 10);
+		short[] sh = new short[5];
+		sh[0] = 1;		//1x1
+		sh[1] = 2;		//2x1
+		sh[2] = 2;		//3x1
+		sh[3] = 2;		//4x1
+		sh[4] = 1;		//5x1
+		char[][] player1 = f.create(12,19);
+		f.place(player1, sh);
 		System.out.println();
 		f.print(player1);
 
