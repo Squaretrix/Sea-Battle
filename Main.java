75f34099a49525a75141aa793fde89067e4f74a8
Patrick S <25529827+Squaretrix@users.noreply.github.com>
2018-09-26 09:32:20 +0200

    V1.02
    Added:
    create() method with width and length parameters

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
-
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Field f = new Field();
 		
-		char[][] player1 = f.create(5);
-		// f.print(player1);
-		f.place(player1, 5);
+		char[][] player1 = f.create(9,12);
+		f.place(player1, 10);
 		System.out.println();
 		f.print(player1);
 
